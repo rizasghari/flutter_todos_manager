60c27dfd16cb005aa3da429c1e0d1272297fb95c
rizasghari <developer.rizasghari@gmail.com>
2025-03-01 17:37:02 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_todos_manager
+
+A minimal cross-platform todos manager application written in Flutter
---
+++ android/app/src/main/kotlin/com/rizasghari/flutter_todos_manager/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.rizasghari.flutter_todos_manager
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
